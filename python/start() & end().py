478f2994437249907a362227432bdd8aa1ba18ff
Ankita17Talwar <90397833+Ankita17Talwar@users.noreply.github.com>
2022-01-11 22:07:37 +0530

    Update start() & end().py
    handled not matched scenario

--- python/start() & end().py
+++ python/start() & end().py
@@ -18,5 +18,10 @@ p = r'(?=(' + pattern +'))'
 
 ms = re.finditer(p,string)
 
+flag=0
 for match in ms:
+    flag =1
     print((match.start(1),match.end(1)-1))
+    
+if flag==0:
+    print((-1,-1))
